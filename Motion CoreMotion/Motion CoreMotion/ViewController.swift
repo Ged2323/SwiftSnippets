a45a09e8ba45997a8db67711209c528414ff973c
Gerard Grundy <uplearnedu@gmail.com>
2018-03-06 06:45:44 +1000

    CoreMotion Update
    Added a label that is moved by coremotion

--- Motion CoreMotion/Motion CoreMotion/ViewController.swift
+++ Motion CoreMotion/Motion CoreMotion/ViewController.swift
@@ -11,6 +11,7 @@ import CoreMotion
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelMoving: UILabel!
     var motionManager = CMMotionManager()
     
     override func viewDidLoad() {
@@ -18,7 +19,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         motionManager = CMMotionManager()
-        motionManager.accelerometerUpdateInterval = 1
+        motionManager.accelerometerUpdateInterval = 0.1
         
         motionManager.startAccelerometerUpdates(to: .main)
         {
@@ -26,8 +27,22 @@ class ViewController: UIViewController {
             if (data?.acceleration) != nil {
                 
                 if (fabs((data?.acceleration.x)!) > 0.2) {
+                    let x = 40.0 * CGFloat((data?.acceleration.x)!)
+                    var y = 40.0 * CGFloat((data?.acceleration.y)!)
+                    print("x—> \(x)\n")
+                    print("y—> \(y)\n")
                     
-                   print("X axis moving—> \(fabs((data?.acceleration.x)!))\n")
+                    if y < 0
+                    {
+                        y = fabs(y)
+                    }
+                    else
+                    {
+                        y = 0 - y
+                    }
+                    
+                   
+                    self?.labelMoving.frame.origin = CGPoint(x: (self?.labelMoving.frame.midX)! + x , y: (self?.labelMoving.frame.midY)! + y)
                    
                     
                     
